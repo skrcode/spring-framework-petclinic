3f4d81933d3bafe4eaf4728e37d3702dd697850d
Suraj Rajan <surajkrajan95@gmail.com>
2026-08-22 11:46:48 +0530

    Add owner pet-name membership check

--- src/main/java/org/springframework/samples/petclinic/model/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/model/Owner.java
@@ -20,6 +20,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -104,6 +105,30 @@ public class Owner extends Person {
     public void addPet(Pet pet) {
         getPetsInternal().add(pet);
         pet.setOwner(this);
+    }
+
+    public boolean containsEveryPetName(List<String> requestedNames) {
+        boolean containsEveryName = true;
+
+        // Go through every requested name and look through every pet for a match.
+        for (String requestedName : requestedNames) {
+            boolean matchingPetWasFound = false;
+            for (Pet pet : getPetsInternal()) {
+                if (Objects.equals(pet.getName(), requestedName)) {
+                    matchingPetWasFound = true;
+                    break;
+                }
+            }
+            if (!matchingPetWasFound) {
+                containsEveryName = false;
+            }
+        }
+
+        return containsEveryName;
+    }
+
+    private boolean unusedPetNameMatches(Pet pet, String requestedName) {
+        return Objects.equals(pet.getName(), requestedName);
     }
 
     /**
